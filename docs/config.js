b619f12fd681b9a71f10ff675591ecc6310eb54d
hfu <hfu@users.noreply.github.com>
2021-04-29 01:30:01 +0000

    update

--- docs/config.js
+++ docs/config.js
@@ -254,6 +254,31 @@ const config = {
         "bearing": 128.2,
         "pitch": 66.0
       }
+    },
+    {
+      "title": "九段会館",
+      "hash": "17.11/35.694094/139.751252/-167.8/57",
+      "id": "chapter-11",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 17.11,
+        "center": [
+          139.751252,
+          35.694094
+        ],
+        "bearing": -167.8,
+        "pitch": 57.0
+      }
     }
   ],
   "footer": "<p>YAML と GitHub Actions と <a href='https://optgeo.github.io/b3p/#15.7/35.680899/139.766841/0/49'>b3p</a>で <a href='https://github.com/mapbox/storytelling'>storytelling</a>しています。</p> <p>このプロジェクトは、 <a href='https://github.com/optgeo'>Adopt Geodata プロジェクト</a>の一環です。</p> <p>地理空間情報は人間の安全保障に貢献するデータですが、 人々のプライバシーや社会の安全に関わる悪用も可能な情報です。</p> <p>適切な<a href='https://ggim.un.org/'>情報管理</a>を行い、 <a href='https://www.mofa.go.jp/mofaj/files/100167362.pdf'>信頼を大切に</a>しながら <a href='https://www.cas.go.jp/jp/seisaku/sokuitiri/index.html'>地理空間情報の活用を推進</a> しましょう。</p>",
