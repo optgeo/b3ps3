51481204977c68d98a5a8e3abb70b9d7bec0973d
hfu <hfu@users.noreply.github.com>
2021-04-29 00:54:43 +0000

    update

---
+++ docs/config.js
@@ -0,0 +1,140 @@
+const config = {
+  "title": "b3ps1",
+  "style": "https://optgeo.github.io/b3p/style.json",
+  "chapters": [
+    {
+      "title": "平成新山",
+      "description": "粘り気の強いデイサイトマグマが、火山の上に盛り上がってできた溶岩ドームです。",
+      "hash": "14.13/32.7633/130.3011/-116.8/60",
+      "id": "chapter-1",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 14.13,
+        "center": [
+          130.3011,
+          32.7633
+        ],
+        "bearing": -116.8,
+        "pitch": 60.0
+      }
+    },
+    {
+      "title": "佐世保工業高等専門学校",
+      "description": "長崎県内唯一の高等専門学校です。",
+      "hash": "17.77/33.149697/129.748356/64.8/62",
+      "id": "chapter-2",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 17.77,
+        "center": [
+          129.748356,
+          33.149697
+        ],
+        "bearing": 64.8,
+        "pitch": 62.0
+      }
+    },
+    {
+      "title": "富士山",
+      "description": "静岡県と山梨県にまたがる活火山です。",
+      "hash": "13.6/35.34613/138.73779/0/82",
+      "id": "chapter-3",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 13.6,
+        "center": [
+          138.73779,
+          35.34613
+        ],
+        "bearing": 0.0,
+        "pitch": 82.0
+      }
+    },
+    {
+      "title": "対馬空港",
+      "description": "愛称は対馬やまねこ空港です。",
+      "hash": "13.84/34.28454/129.33033",
+      "id": "chapter-4",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 13.84,
+        "center": [
+          129.33033,
+          34.28454
+        ],
+        "bearing": null,
+        "pitch": null
+      }
+    },
+    {
+      "title": "奥多摩湖",
+      "description": "正式名称は小河内貯水池です。",
+      "hash": "14.95/35.7801/139.03488/173.9/59",
+      "id": "chapter-5",
+      "alignment": "right",
+      "callback": null,
+      "hidden": false,
+      "mapAnimation": "flyTo",
+      "rotateAnimation": true,
+      "onChapterEnter": [
+
+      ],
+      "onChapterExit": [
+
+      ],
+      "location": {
+        "zoom": 14.95,
+        "center": [
+          139.03488,
+          35.7801
+        ],
+        "bearing": 173.9,
+        "pitch": 59.0
+      }
+    }
+  ],
+  "footer": "<p>YAML と GitHub Actions と <a href='https://optgeo.github.io/b3p/#15.7/35.680899/139.766841/0/49'>b3p</a>で <a href='https://github.com/mapbox/storytelling'>storytelling</a>しています。</p> <p>このプロジェクトは、 <a href='https://github.com/optgeo'>Adopt Geodata プロジェクト</a>の一環です。</p> <p>地理空間情報は人間の安全保障に貢献するデータですが、 人々のプライバシーや社会の安全に関わる悪用も可能な情報です。</p> <p>適切な<a href='https://ggim.un.org/'>情報管理</a>を行い、 <a href='https://www.mofa.go.jp/mofaj/files/100167362.pdf'>信頼を大切に</a>しながら <a href='https://www.cas.go.jp/jp/seisaku/sokuitiri/index.html'>地理空間情報の活用を推進</a> しましょう。</p>",
+  "accessToken": "pk.eyJ1IjoiaGZ1IiwiYSI6ImlRSGJVUTAifQ.rTx380smyvPc1gUfZv1cmw",
+  "theme": "light",
+  "showMarkers": false
+}
